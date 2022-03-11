4f9139875854503beaa42f36130ecb919c0944b5
Antonio Borondo <antonioborondo@users.noreply.github.com>
2022-03-11 14:00:16 +0100

    Get info from webcam (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <opencv2/opencv.hpp>
 
-int main()
+#include <iostream>
+#include <string>
+
+int main(int argc, const char** argv)
 {
-    cv::VideoCapture video_capture(0);
+    if(argc < 2)
+    {
+        std::cout << "Usage: webcam_settings.exe <device> [<exposure>]" << std::endl;
+
+        return -1;
+    }
+
+    const auto device{std::stoi(argv[1])};
+    cv::VideoCapture video_capture(device);
+    if(!video_capture.isOpened())
+    {
+        std::cout << "Error: Cannot open device " << device << std::endl;
+
+        return -1;
+    }
+
+    auto exposure{video_capture.get(cv::CAP_PROP_EXPOSURE)};
+
+    std::cout << "Default exposure: " << exposure << std::endl;
+
+    if(argc > 2)
+    {
+        exposure = std::stod(argv[2]);
+    }
+
+    video_capture.set(cv::CAP_PROP_EXPOSURE, exposure);
+
+    std::cout << "New exposure: " << exposure << std::endl;
 
     return 0;
 }
