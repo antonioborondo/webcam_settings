e265a6a9871ea7fb081c09963722dd1eefb6d51d
Antonio Borondo <antonioborondo@outlook.com>
2022-03-11 11:51:52 +0100

    Draw video

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <opencv2/opencv.hpp>
 
+#include <string>
+
 int main()
 {
     cv::VideoCapture video_capture(0);
+    if(!video_capture.isOpened())
+    {
+        return -1;
+    }
+
+    std::string window_name{"Video"};
+    cv::namedWindow(window_name, 1);
+
+    for(;;)
+    {
+        cv::Mat frame;
+        video_capture >> frame;
+
+        cv::imshow(window_name, frame);
+
+        if(cv::waitKey(30) >= 0)
+        {
+            break;
+        }
+    }
 
     return 0;
 }
