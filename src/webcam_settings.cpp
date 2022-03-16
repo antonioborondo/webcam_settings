8f72bbcdfad70407f601b5b66c1a687beba464d9
Antonio Borondo <antonioborondo@outlook.com>
2022-03-16 22:46:36 +0100

    Clean up code

--- src/webcam_settings.cpp
+++ src/webcam_settings.cpp
@@ -1,23 +1,23 @@
 #include "webcam_settings.h"
-#include "./ui_webcam_settings.h"
+
+#include "ui_webcam_settings.h"
 #include "video_capture_devices.h"
 
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
-#include <string>
 
 Webcam_settings::Webcam_settings(const std::vector<std::wstring> &video_capture_devices_list, QWidget *parent)
     : m_video_capture_devices_list{video_capture_devices_list}
-     , QMainWindow(parent)
+    , QMainWindow(parent)
     , ui(new Ui::Webcam_settings)
 {
     ui->setupUi(this);
-    for(const auto device : m_video_capture_devices_list)
-    {
-        ui->m_devices->addItem(QString::fromStdWString(device));
-    }
 
+    for(const auto video_capture_devices : m_video_capture_devices_list)
+    {
+        ui->m_devices->addItem(QString::fromStdWString(video_capture_devices));
+    }
 }
 
 Webcam_settings::~Webcam_settings()
